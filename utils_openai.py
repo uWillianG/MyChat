e3727b54edcefcc556e08478dc204d921ed8f96c
Willian Siqueira <willian.gabriel.siq@gmail.com>
2025-03-12 22:55:09 -0300

    Update utils_openai.py

--- utils_openai.py
+++ utils_openai.py
@@ -1,6 +1,8 @@
 import openai
-from dotenv import load_dotenv, find_dotenv
+import requests
+from PIL import Image
 
+from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
 client = openai.Client()
@@ -15,3 +17,31 @@ def retorna_resposta_modelo(mensagens,
         stream=stream
     )
     return response
+
+def retorna_imagem(prompt,
+                   nome_arquivo,
+                   modelo='dall-e-3'
+                   ):
+    resposta_imagem=client.images.generate(model=modelo,
+                                prompt=prompt,
+                                #'Você é um gerador de imagens, \
+                                #realize a geração conforme a solicitação do usuário: {solicitacao_imagem}',
+                                size='1024x1024',
+                                quality = 'standard',
+                                style = 'natural',
+                                n=1
+                            )
+    
+    # Obtém a URL da imagem gerada
+    if resposta_imagem and resposta_imagem.data and len(resposta_imagem.data) > 0:
+        image_url = resposta_imagem.data[0].url
+    else:
+        return None  # Se a resposta não contém imagem, retorna None
+
+    # Baixa e salva a imagem
+    img_data = requests.get(image_url).content
+    with open(nome_arquivo, 'wb') as f:
+        f.write(img_data)
+
+    return image_url  # Retorna a URL correta da imagem gerada
+
